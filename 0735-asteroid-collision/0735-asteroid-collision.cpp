0516c9680079e7ad2ab77746fdccc7b8cb5808d0
Ayush kamboj <152530498+Ayushh-k@users.noreply.github.com>
2026-09-21 17:54:49 +0530

    Time: 0 ms (100%), Space: 22.1 MB (57.58%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,27 +4,30 @@ public:
         stack<int> s;
 
         for(auto as : asteroids){
-            if(as > 0) {
-                 s.push(as);
+            if(as > 0){
+                s.push(as);
             }else{
-                while(!s.empty() && as < 0 && s.top() < -as && s.top()> 0){
+                while(!s.empty() && as < 0 && s.top() > 0 && s.top() < -as){
                     s.pop();
                 }
 
-
                 if(!s.empty() && s.top() == -as){
                     s.pop();
-                }else if(s.empty() || s.top() < 0){
+                }
+
+                else if(s.empty() || s.top() < 0){
                     s.push(as);
                 }
             }
         }
 
         vector<int> ans(s.size());
-        for(int i=s.size()-1;i>=0; i--){
+
+        for(int i = s.size()-1; i>=0; i--){
             ans[i] = s.top();
             s.pop();
         }
+
         return ans;
     }
 };
